9876c5f822230de55d6d146c39b8c24cb12ce565
Hoff97 <frithjof97@web.de>
2013-10-22 22:29:58 +0200

    Whoops^^

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 	if($_POST["type"]=="input")
 	{
 		$connect = mysql_connect('server', 'user', 'pw');
-		mysql_select_db("DB494234", $connect);
+		mysql_select_db("db", $connect);
 		echo $_POST["content"];
 		$sql = "INSERT INTO messages VALUES ('" . $_POST["content"] . "')";
 		mysql_query($sql,$connect);
@@ -10,7 +10,7 @@
 	else if($_POST["type"]=="output")
 	{
 		$connect = mysql_connect('Server', 'User', 'PW');
-		mysql_select_db("DB494234", $connect);
+		mysql_select_db("db", $connect);
 		$query = "SELECT * FROM messages";
 		$result = mysql_query($query,$connect);
 		
