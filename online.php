7a96ff4ae361d47da319d2b7b4cf22b3a3ed380f
Hoff97 <frithjof97@web.de>
2013-10-25 15:48:51 +0200

    Added A Onlinelist at the Right Hand Side

---
+++ online.php
@@ -0,0 +1,41 @@
+<?php
+	$connect = mysql_connect('localhost', 'root', 'Jan2008');
+	mysql_select_db("savechat", $connect);
+	
+	$date = (new DateTime())->sub(new DateInterval("PT30S"));
+	$date = $date->format('YmdHis');
+	$sql = "DELETE FROM `online` WHERE `date` < ".$date;
+	mysql_query($sql,$connect);
+	
+	$query = "SELECT * FROM online";
+	$result = mysql_query($query,$connect);
+	$in = false;
+	
+	echo "[";
+	$a = 0;
+	while($row = mysql_fetch_array($result))
+	{
+		if($_POST["name"]==$row["name"])
+		{
+			$query = "UPDATE `online` SET `date` = CURRENT_TIMESTAMP WHERE `name` = '".$_POST["name"]."'";
+			mysql_query($query,$connect);
+			$in = true;
+		}
+		else
+		{
+			if($a>0)
+			{
+				echo ",";
+			}
+			echo "{\"name\":\"".$row["name"]."\"}";
+			$a++;
+		}
+	}
+	echo "]";
+	
+	if($in==false)
+	{
+		$query = "INSERT INTO `savechat`.`online` (`name`,`date`) VALUES ('".$_POST["name"]."', CURRENT_TIMESTAMP);";
+		mysql_query($query,$connect);
+	}
+?>
