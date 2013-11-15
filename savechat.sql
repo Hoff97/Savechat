fc98dd7f0f5381ba816a06f36e4c7e4a9638d10a
Hoff97 <frithjof97@web.de>
2013-11-15 14:42:59 +0100

    Updated SQL

--- savechat.sql
+++ savechat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 22. Okt 2013 um 22:57
+-- Erstellungszeit: 15. Nov 2013 um 14:41
 -- Server Version: 5.5.27
 -- PHP-Version: 5.4.7
 
@@ -27,16 +27,28 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `messages` (
-  `content` varchar(10000) NOT NULL
+  `content` varchar(10000) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `messages`
 --
+-- --------------------------------------------------------
 
-INSERT INTO `messages` (`content`) VALUES
-('}%jwqp....AND........QUEST....$vk....BRACKET.....%gtpwisv%>....BRACKET....Jdpqq%Â'),
-('dsf');
+--
+-- Tabellenstruktur für Tabelle `online`
+--
+
+CREATE TABLE IF NOT EXISTS `online` (
+  `name` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `online`
+--
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
